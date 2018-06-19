b28041abf79fdf2c0fc8871aed794179f48268b5
Hannah Carnes <hecarnes@gmail.com>
2018-06-19 19:08:32 -0400

    print start time and simple styling enhancements

--- calendarfrontend/src/components/EventItem.jsx
+++ calendarfrontend/src/components/EventItem.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import format from 'date-fns/format'
 
 class EventItem extends Component {
 
   render() {
     return (
       <li>
-        {this.props.event.description}
+        <div className="eventItemElement">{this.props.event.description}</div>
+        <div className="eventItemElement">
+          {format(this.props.event.start, "H:mm")}
+        </div>
       </li>
     );
   }
