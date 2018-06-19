2b027d0f71ff344a9563482bce5862d3b15708df
Hannah Carnes <hecarnes@gmail.com>
2018-06-18 21:07:33 -0400

    print days of month

--- calendarfrontend/src/components/Calendar.jsx
+++ calendarfrontend/src/components/Calendar.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Month from './Month';
 import addMonths from 'date-fns/add_months'
+import startOfMonth from 'date-fns/start_of_month'
 
 class Calendar extends Component {
   constructor() {
     super();
     this.state = {
-      selectedMonth: new Date()
+      selectedMonth: startOfMonth(new Date())
     };
   }
   nextMonth = () => {
--- calendarfrontend/src/components/Month.jsx
+++ calendarfrontend/src/components/Month.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import format from 'date-fns/format'
-import addMonths from 'date-fns/add_months'
+import lastDayOfMonth from 'date-fns/last_day_of_month'
+import eachDay from 'date-fns/each_day'
 
 class Month extends Component {
   formatMonth = () => {
     return format(this.props.date, "MMMM");
   }
 
+  renderDays = () => {
+    const endOfMonth = lastDayOfMonth(this.props.date);
+    const daysOfMonth = eachDay(this.props.date, endOfMonth)
+
+    return daysOfMonth.map(date => {
+      return (
+        <div>
+          Day: {date.getDate()}
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
-        {this.formatMonth()}
+        <div>{this.formatMonth()}</div>
+        <div>{this.renderDays()}</div>
       </div>
     );
   }
