03cd9be5721854e823fa490c99f19ff00c3d1ff9
Hannah Carnes <hecarnes@gmail.com>
2018-06-19 19:34:13 -0400

    Convert date strings to js dates on load

--- calendarfrontend/src/reducers.js
+++ calendarfrontend/src/reducers.js
@@ -1,11 +1,23 @@
+import parse from 'date-fns/parse'
 export default function eventsReducer(state = {
   events: []
 }, action) {
+  const convertDates = (events) => {
+    return events.map(event => {
+      return {
+        ...event,
+        start: parse(event.start),
+        end: parse(event.start),
+        created_at: parse(event.start),
+        updated_at: parse(event.start),
+      }
+    });
+  };
   switch (action.type) {
     case "ADD_EVENT":
-      return { ...state, events: [...state.events, action.event] };
+      return { ...state, events: [...state.events, ...convertDates([action.event])] };
     case "ADD_EVENTS":
-      return { ...state, events: action.events };
+      return { ...state, events: convertDates(action.events) };
     default:
       console.debug(action);
       return state;
