980448ec33930e577ee9ce1aec608256f9fb7857
Hannah Carnes <hecarnes@gmail.com>
2018-06-18 21:36:17 -0400

    add create event form placeholder

--- calendarfrontend/src/components/Day.jsx
+++ calendarfrontend/src/components/Day.jsx
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 
 class Day extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false
+    };
+  }
+
+  closeCreateEventForm = () => {
+    this.setState({ modalIsOpen: false });
+  }
+
   showCreateEventForm = () => {
-    alert("new event form goes here")
+    this.setState({ modalIsOpen: true });
+  }
+
+  createEvent = (ev) => {
+    ev.preventDefault();
+    this.closeCreateEventForm();
   }
 
   render() {
     return (
       <li onClick={this.showCreateEventForm}>
         {this.props.date.getDate()}
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeCreateEventForm}
+          contentLabel="Example Modal"
+        >
+          Hello World
+
+          <form onSubmit={this.createEvent}>
+            Event goes here
+            <input type="submit" value="Create Event" />
+          </form>
+        </Modal>
       </li>
     );
   }
