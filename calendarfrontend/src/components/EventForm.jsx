2fb010f295872e808e095445aad740517f26b837
Hannah Carnes <hecarnes@gmail.com>
2018-06-19 20:03:00 -0400

    track form changes in state

--- calendarfrontend/src/components/EventForm.jsx
+++ calendarfrontend/src/components/EventForm.jsx
@@ -4,17 +4,31 @@ import { bindActionCreators } from 'redux';
 import { addEvent } from '../actions/addEvent';
 
 class EventForm extends Component {
+  constructor(props) {
+    super(props);
+
+    if (props.event) {
+      this.state = {
+        event: Object.assign({}, props.event)
+      }
+    } else {
+      this.state = { event: {} }
+    }
+  }
 
   handleOnSubmit = (event) => {
     event.preventDefault();
-    const eventData = new FormData(event.target);
-    const eventObject = {};
-    for (const [key, value] of eventData.entries()) {
-      eventObject[key] = value;
-    }
 
-    this.props.addEvent({ ...eventObject, date: this.props.selectedDate });
+    this.props.addEvent({ ...this.state.event, date: this.props.selectedDate });
     this.props.onSuccess();
+  }
+
+  handleChange = (ev) => {
+    const event = {
+      ...this.state.event,
+      [ev.target.name]: ev.target.value
+    }
+    this.setState({ event });
   }
 
   render() {
@@ -23,15 +37,15 @@ class EventForm extends Component {
         New Event on {this.props.selectedDate.toString()}
         <p>
           <label htmlFor="description">Description</label>
-          <input type="text" id="description" name="description" />
+          <input type="text" id="description" name="description" value={this.state.event.description} onChange={this.handleChange} />
         </p>
         <p>
           <label htmlFor="startTime">Start Time</label>
-          <input type="time" id="startTime" name="startTime" />
+          <input type="time" id="startTime" name="startTime" value={this.state.event.startTime} onChange={this.handleChange} />
         </p>
         <p>
           <label htmlFor="endTime">End Time</label>
-          <input type="time" id="endTime" name="endTime" />
+          <input type="time" id="endTime" name="endTime" value={this.state.event.endTime} onChange={this.handleChange} />
         </p>
         <p>
           <input type="submit" value="Create Event" />
